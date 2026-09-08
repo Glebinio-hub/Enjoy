bfafbea2952c504cb3485f8353be11adaed3024b
Glebinio <98263503+Glebinio-hub@users.noreply.github.com>
2026-09-08 11:16:24 +0300

    UpdateRelayCommand

--- EnjoyApp/EnjoyApp/ViewModels/RelayCommand.cs
+++ EnjoyApp/EnjoyApp/ViewModels/RelayCommand.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace EnjoyApp.ViewModels
 {
-    internal class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parametr)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
     }
+
 }
+
+
